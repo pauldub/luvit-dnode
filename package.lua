80c621287df015a09dc97bf4e7b537eae437a7cf
Paul d'Hubert <paul@tymate.com>
2014-03-29 14:19:15 +0100

    Fix package.lua

--- package.lua
+++ package.lua
@@ -1,7 +1,7 @@
 return { 
-  name = 'test',
+  name = 'dnode',
   version = '0.0.1', 
-  description = 'test the awesome [...] made with luve', 
+  description = 'Substack\'s dnode ported to luvit.', 
   author = "Paul d'Hubert <paul.dhubert@yandex.ru>", 
   dependencies = { 
 		['bourbon'] = 'https://github.com/racker/luvit-bourbon/archive/master.zip'
