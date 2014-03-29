3f116e6319aeb5ca6eeed08c8676e9a99269d9cc
Paul d'Hubert <paul@tymate.com>
2014-03-29 14:51:37 +0100

    Add async to package.lua so bourbon works?

--- package.lua
+++ package.lua
@@ -4,6 +4,7 @@ return {
   description = 'Substack\'s dnode ported to luvit.', 
   author = "Paul d'Hubert <paul.dhubert@yandex.ru>", 
   dependencies = { 
+		['async'] = 'https://github.com/virgo-agent-toolkit/luvit-async/archive/master.zip',
 		['bourbon'] = 'https://github.com/racker/luvit-bourbon/archive/master.zip'
   },
  }
