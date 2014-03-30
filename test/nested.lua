135f64bee980ae9c9a91dcdaca9337f5c27e950e
Paul d'Hubert <paul@tymate.com>
2014-03-30 15:11:50 +0200

    Add nested test.

---
+++ test/nested.lua
@@ -0,0 +1,77 @@
+local exports = {}
+
+local dnode = require('..')
+local utils = require('utils')
+local asserts = require('bourbon/lib/asserts')
+
+local timer = require('timer')
+local Emitter = require('core').Emitter
+
+-- This test is not really readable, there is probably something
+-- wrong with dnode:listen, I have to close the dnode connections
+-- and then close the tcp connections.
+
+exports['test_nested'] = function(test)
+	local server1 
+	local net1 = dnode:listen(function(self, remote)
+		server1 = self
+
+		return {
+			timesTen = function(n, reply)
+				reply(n * 10)
+			end
+		}
+	end, 1337).net
+
+	local server2 
+	local net2 = dnode:listen(function(self, remote)
+		server2 = self
+
+		return {
+			timesTwenty = function(n, reply)
+				reply(n * 20)
+			end
+		}
+	end, 1338).net
+
+	local moo = Emitter:new()
+
+	local client1 = dnode:connect(1337, function(remote1, conn)
+		local client2 = dnode:connect(1338, function(remote2, conn)
+			moo:on('hi', function(x)
+				remote1.timesTen(x, function(res)
+					asserts.equals(res, 5000, 'emitted value times ten')
+
+					remote2.timesTwenty(res, function(res2)
+						asserts.equals(res2, 100000, 'result times twenty')
+
+						moo:emit('end')
+					end)
+				end)
+			end)
+
+			remote2.timesTwenty(5, function(n)
+				asserts.equals(n, 100)
+
+				remote1.timesTen(0.1, function(n)
+					asserts.equals(n, 1)
+				end)
+			end)
+		end)
+	end)
+
+	moo:on('end', function()
+		server1:destroy()
+		server2:destroy()
+		net1:close()
+		net2:close()
+		test.done()
+	end)
+
+	timer.setTimeout(200, function()
+		moo:emit('hi', 500)
+	end)
+end
+
+return exports
+
