e20bb00d374e2629a091fe03c9bbabf628676d90
Paul d'Hubert <paul@tymate.com>
2014-03-30 14:16:35 +0200

    Add double test, this is a funny one.

---
+++ test/double.lua
@@ -0,0 +1,43 @@
+local exports = {}
+
+local dnode = require('..')
+local utils = require('utils')
+local asserts = require('bourbon/lib/asserts')
+
+exports['test_double'] = function(test)
+	local server = dnode:listen({
+		z = function(f, g ,h)
+			f(10, function(x)
+				g(10, function(y)
+					h(x, y)
+				end)
+			end)
+		end
+	}, 1337)
+
+	local client = dnode:connect(1337, function(remote, conn)
+		remote.z(function(x, f)
+			f(x * 2)
+		end, function(x, f)
+			f(x / 2)
+		end, function(x, y)
+			asserts.equals(x, 20, 'double not equal')
+			asserts.equals(y, 5, 'double not equal')
+		end)
+
+		local plusTen = function(n, f)
+			f(n + 10)
+		end
+
+		remote.z(plusTen, plusTen, function(x, y)
+			asserts.equals(x, 20, 'double not equal')
+			asserts.equals(y, 20, 'double not equal')
+
+			server.net:close()
+			test.done()
+		end)
+	end)
+end
+
+return exports
+
