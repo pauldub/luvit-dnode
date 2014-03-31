82ff9bd8797318bbdbfee5dd0539a52ba57b9729
Paul d'Hubert <paul.dhubert@yandex.ru>
2014-03-31 20:51:29 +0200

    Correctly call block with callbacks et al

--- test/simple_kv.lua
+++ test/simple_kv.lua
@@ -7,21 +7,31 @@ local asserts = require('bourbon/lib/asserts')
 
 local function KvServer()
   local db = { }
-  local interface = {
+  local self = dnode:new({
 	  load = function(key, reply)
       reply(db[key])
 	  end,
 	
-	  fetch = function(key, reply, block)-- ... --[[ options, block ]]) -- block is a function called if the key 
-                                                             -- is not available and return its return 
-	                                                           -- value. If block is not a function it is
-                                                             -- used as is.
+    -- block is a function called if the key 
+    -- is not available and return its return 
+    -- value. If block is not a function it is
+    -- used as is.
+	  fetch = function(key, reply, block)-- ... --[[ options, block ]]) 
       local value = db[key]
       if value == nil then
-         value = type(block) == 'function' and block() or block
-         db[key] = value
+        if type(block) == 'function' then
+          block(function(block_result)
+             db[key] = block_result 
+             reply(block_result) 
+          end) 
+        else 
+          db[key] = block
+
+          reply(block)
+        end
+      else
+        reply(value) 
       end
-      reply(value) 
     end,
 
     store = function(key, value, reply) -- ... --[[ options, reply ]])
@@ -43,10 +53,10 @@ local function KvServer()
     has_key = function(key, reply)
       reply(db[key] ~= nil)
     end,
-  }
+  })
 
-  local self = dnode:new(interface)
   self.db = db
+
   return self
 end
 
@@ -73,14 +83,75 @@ exports['test_kv_server_fetch'] = function(test)
 
   local client = dnode:new()
   client:on('remote', function(remote, conn)
-    remote.fetch('foo', function(old_val)
-      asserts.equals(old_val, 'default')
+    remote.store('foo', 'bar', function()
+      remote.fetch('foo', function(val)
+        asserts.equals(val, 'bar')
+        asserts.equals(server.db.foo, 'bar')
+
+        conn:destroy()
+        server:destroy()
+        test.done()
+      end)
+    end)
+  end)
+
+  client:pipe(server)
+  server:pipe(client)
+end
+
+exports['test_kv_server_fetch_returns_nil'] = function(test)
+	local server = KvServer()
+
+  local client = dnode:new()
+  client:on('remote', function(remote, conn)
+    remote.fetch('foo', function(val)
+      asserts.equals(val, nil)
+      asserts.equals(server.db.foo, nil)
+
+      conn:destroy()
+      server:destroy()
+      test.done()
+    end)
+  end)
+
+  client:pipe(server)
+  server:pipe(client)
+end
+
+exports['test_kv_server_fetch_block_value'] = function(test)
+	local server = KvServer()
+
+  local client = dnode:new()
+  client:on('remote', function(remote, conn)
+    remote.fetch('foo', function(val)
+      asserts.equals(val, 'default')
       asserts.equals(server.db.foo, 'default')
 
       conn:destroy()
       server:destroy()
       test.done()
     end, 'default')
+  end)
+
+  client:pipe(server)
+  server:pipe(client)
+end
+
+exports['test_kv_server_fetch_block_function'] = function(test)
+	local server = KvServer()
+
+  local client = dnode:new()
+  client:on('remote', function(remote, conn)
+    remote.fetch('foo', function(val)
+      asserts.equals(val, 'default')
+      asserts.equals(server.db.foo, 'default')
+
+      conn:destroy()
+      server:destroy()
+      test.done()
+    end, function(reply)
+      reply('default')
+    end)
   end)
 
   client:pipe(server)
