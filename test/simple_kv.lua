7b6b6facbbd2f66548580cd688a2ad35a622ccc2
Paul d'Hubert <paul.dhubert@yandex.ru>
2014-03-31 20:31:45 +0200

    Add simple key-value db server test

---
+++ test/simple_kv.lua
@@ -0,0 +1,72 @@
+local exports = {}
+
+local dnode = require('..')
+local Object = require('core').Object
+local utils = require('utils')
+local asserts = require('bourbon/lib/asserts')
+
+local function KvServer()
+  local db = { }
+  local interface = {
+	  load = function(key, reply)
+      reply(db[key])
+	  end,
+	
+	  fetch = function(key, reply, block)-- ... --[[ options, block ]]) -- block is a function called if the key 
+                                                             -- is not available and return its return 
+	                                                           -- value. If block is not a function it is
+                                                             -- used as is.
+      local value = db[key]
+      if value == nil then
+         value = type(block) == 'function' and block() or block
+         db[key] = value
+      end
+      reply(value) 
+    end,
+
+    store = function(key, value, reply) -- ... --[[ options, reply ]])
+      local old_value = db[key]
+
+      db[key] = value
+
+      reply(old_value)
+    end,
+
+    delete = function(key, reply) --... --[[ options, reply ]])
+      local value = db[key]
+
+      db[key] = nil
+      
+      reply(value)
+    end,
+
+    has_key = function(key, reply)
+      reply(db[key] ~= nil)
+    end,
+  }
+
+  local self = dnode:new(interface)
+  self.db = db
+  return self
+end
+
+exports['test_kv_server_can_listen'] = function(test)
+	local server = KvServer()
+
+  server:listen(0, function()
+	  local client = dnode:new()
+    client:connect(server.net:address().port, function(remote, conn)
+      print(utils.dump(server))      
+      remote.fetch('foo', function(old_val)
+        asserts.equals(old_val, 'default')
+        asserts.equals(server.db.foo, 'default')
+
+        conn:destroy()
+        server:destroy()
+        test.done()
+      end, 'default')
+	  end)
+  end)
+end
+
+return exports
